3dc55a522385d2b63e680e7c93221d3f71ec20a2
Niko Heikkilä <niko.heikkila@futurice.com>
2022-06-13 20:03:35 +0300

    feat: add route `/movies/add` to add new movies

--- movies_api/api.py
+++ movies_api/api.py
@@ -1,10 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field, StrictBool
 
-StandardResponse = dict[str, str]
+from movies_api.service import MovieService
+from movies_api.repository import MovieRepository
+from movies_api.models import Movie
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root() -> StandardResponse:
-    return {"title": "Movies API"}
+class HealthCheckResponse(BaseModel):
+    code: int = Field(..., ge=0, description="Health check response code")
+
+
+class AddMoviesRequest(BaseModel):
+    name: str = Field(..., min_length=1, description="Movie title")
+    year: int = Field(..., ge=1900, le=2050, description="Movie release year")
+    rating: float = Field(..., ge=0.0, le=10.0, description="Movie rating")
+
+
+class AddMoviesResponse(BaseModel):
+    success: StrictBool = Field(default=True, description="Success flag")
+    movies: list[str] = Field(default=[], description="List of added movies")
+
+
+@app.get("/healthcheck", response_model=HealthCheckResponse)
+def healthcheck() -> HealthCheckResponse:
+    return HealthCheckResponse(code=0)
+
+
+@app.post("/movies/add", status_code=201, response_model=AddMoviesResponse)
+def add_movies(request: list[AddMoviesRequest]) -> AddMoviesResponse:
+    service = MovieService(MovieRepository.with_memory())
+    models = [Movie(**item.dict()) for item in request]
+
+    service.add_movies(*models)
+    movies = service.list()
+
+    return AddMoviesResponse(success=True, movies=[movie.name for movie in movies])
--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -11,8 +11,28 @@ def client() -> TestClient:
 
 
 class TestMoviesAPI:
-    def test_root_route_should_return_ok_response(self, client: TestClient) -> None:
-        response = client.get("/")
+    def test_healthcheck_route_should_return_zero_exit_code_for_healthy_system(self, client: TestClient) -> None:
+        response = client.get("/healthcheck")
 
-        assert_that(response.status_code).is_equal_to(200)
-        assert_that(response.json()).contains_key("title").contains_value("Movies API")
+        assert_that(response).has_status_code(200)
+        assert_that(response.json()).is_equal_to(
+            {
+                "code": 0,
+            }
+        )
+
+    def test_add_movies_should_return_the_movies_added(self, client: TestClient) -> None:
+        payload = [
+            {"name": "The Shawshank Redemption", "year": 1994, "rating": 9.3},
+            {"name": "The Godfather", "year": 1972, "rating": 9.2},
+        ]
+
+        response = client.post("/movies/add", json=payload)
+
+        assert_that(response).has_status_code(201)
+        assert_that(response.json()).is_equal_to(
+            {
+                "success": True,
+                "movies": ["The Shawshank Redemption", "The Godfather"],
+            }
+        )
